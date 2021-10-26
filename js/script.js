9dc5f1cad08405132b3b7d89fd714c3ac4bfed29
Cristian1231233 <campagnacristian22@gmail.com>
2021-10-26 15:42:31 +0200

    altro commit

--- js/script.js
+++ js/script.js
@@ -15,11 +15,34 @@ const listEmail = ['Giovanni2@gmail.com', 'Sara2@gmail.com', 'Marco3@gmail.com',
 console.log(listEmail);
 const emailValida = 'false';
 
-for(let i = 0; i < (listEmail.length); i++){
+for(let i = 0; i < listEmail.length; i++){
     console.log(listEmail[i]);
 
 if(emailUtente === listEmail[i]){
     emailValida = 'true';
-    console.log('email trovata');
+    console.log('emailValida');
 }
+}
+// if(emailValida == true){
+//     console.log('puoi accedere');
+// }else{
+//     console.log('non puoi accedere');
+// }
+
+
+//Giocatore e computer generano un numero da 1 a 6
+// Vince il punteggio più alto
+
+let numPlayer = Math.floor(Math.random() *6) +1;
+console.log(numPlayer);
+
+let numPc = Math.floor(Math.random() *6) +1;
+console.log(numPc);
+
+if(numPlayer > numPc){
+    console.log('vince numPlayer');
+}else if(numPlayer < numPc){
+    console.log('vince numPc');
+}else{
+    console.log('pareggio');
 }
